4133942d7120d4eda3b9ec86056db6c7bd2e49e6
Antoine Chauvin <antoine@chauvin.online>
2018-02-06 18:47:48 +0100

    Start sessions

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   resources :cert_profiles
   resources :authorities
   resources :users
+
+  resource :session, only: [:new, :create, :destroy]
+
+  root to: 'session#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
