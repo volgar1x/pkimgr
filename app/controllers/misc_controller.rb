5216b07873c90d1a5fe492a79c298f38b191184d
Antoine Chauvin <antoine@chauvin.online>
2018-02-08 15:12:08 +0100

    Dashboard page

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -1,4 +1,10 @@
 class MiscController < SecureController
   def dashboard
+    @versions = {
+      openssl: OpenSSL::OPENSSL_VERSION.split(' ')[1],
+      ruby: `ruby -v`.split(' ')[1],
+      rails: `rails -v`.split(' ')[1],
+      postgres: `postgres -V`.split(' ')[2],
+    }
   end
 end
