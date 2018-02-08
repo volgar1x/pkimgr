4dbd2958e05f87e4dfc006329687081e0fe3d4ec
Antoine Chauvin <antoine@chauvin.online>
2018-02-08 10:51:13 +0100

    Do not use CoffeeScript & Turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
-//= require jquery-slim
-//= require popper
-//= require bootstrap
+//= require dist/jquery-slim
+//= require dist/popper
+//= require dist/bootstrap
 //= require_tree .
