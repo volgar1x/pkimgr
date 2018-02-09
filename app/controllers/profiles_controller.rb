74a1de295464959854cf60ba765fc91015eaff8e
Antoine Chauvin <antoine@chauvin.online>
2018-02-09 10:41:21 +0100

    Profile Update complete

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,8 +3,22 @@ class ProfilesController < SecureController
   end
 
   def update
+    respond_to do |format|
+      if current_user.update(profile_params)
+        format.html { redirect_to root_path, notice: 'Your profile was successfully updated.' }
+        format.json { render :show, status: :ok, location: profile_path }
+      else
+        format.html { render :edit }
+        format.json { render json: current_user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+  end
+
+private
+  def profile_params
+    params.require(:profile).permit(:email, :firstname, :lastname, :street, :street2, :city, :zip, :country, :phone)
   end
 end
--- app/controllers/secure_controller.rb
+++ app/controllers/secure_controller.rb
@@ -1,6 +1,11 @@
 class SecureController < ApplicationController
   before_action :require_login
 
+protected
+  def current_user
+    @current_user ||= User.find(session[:current_user])
+  end
+
 private
   def require_login
     redirect_to new_session_path unless session[:current_user]
