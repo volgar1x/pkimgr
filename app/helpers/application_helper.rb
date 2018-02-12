f5825395904cc93b70134cbf3f3752eca912a645
Antoine Chauvin <antoine@chauvin.online>
2018-02-12 12:54:12 +0100

    Split navbar and flashes into their own partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,26 @@ module ApplicationHelper
   def current_user
     @current_user ||= User.find(session[:current_user])
   end
+
+  def nav_item(body, path, options = {}, &block)
+    if block
+      options = path
+      path = body
+      body = block.call
+    end
+
+    classes = "nav-item"
+    if path == request.fullpath
+      classes += " active"
+    end
+
+    content_tag(:li, body, class: classes)
+  end
+
+  def nav_link(name, path, options = {}, &block)
+    options[:class] ||= ""
+    options[:class] += " nav-link"
+
+    nav_item(link_to(name, path, options, &block), path)
+  end
 end
