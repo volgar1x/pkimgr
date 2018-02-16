ace26a76cb9e6bac6a46b6d6fd3a312138cce9d1
Antoine Chauvin <antoine@chauvin.online>
2018-02-16 11:37:18 +0100

    Do not crash on dashboard

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,9 +2,9 @@ class MiscController < SecureController
   def dashboard
     @versions = {
       openssl: OpenSSL::OPENSSL_VERSION.split(' ')[1],
-      ruby: `ruby -v`.split(' ')[1],
-      rails: `rails -v`.split(' ')[1],
-      postgres: `postgres -V`.split(' ')[2],
+      ruby: `ruby -v`.try(:split).try(:at, 1) || "unknown",
+      rails: `rails -v`.try(:split).try(:at, 1) || "unknown",
+      postgres: `postgres -V`.try(:split).try(:at, 2) || "unknown",
     }
   end
 end
