b55046cb675f174d2861b98cdbef295db9950bf6
Antoine Chauvin <antoine@chauvin.online>
2018-02-21 19:22:05 +0100

    Improve Dashboard

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,9 +2,19 @@ class MiscController < SecureController
   def dashboard
     @versions = {
       openssl: OpenSSL::OPENSSL_VERSION.split(' ')[1],
-      ruby: `ruby -v`.try(:split).try(:at, 1) || "unknown",
-      rails: `rails -v`.try(:split).try(:at, 1) || "unknown",
-      postgres: `postgres -V`.try(:split).try(:at, 2) || "unknown",
+      ruby: "#{Object::RUBY_ENGINE} #{Object::RUBY_ENGINE_VERSION}",
+      rails: Rails.version,
+      postgres: postgres_version,
     }
   end
+
+  private
+    def postgres_version
+      v = ApplicationRecord.connection.postgresql_version
+      [
+        v / 100_00,
+        v / 100 % 100,
+        v % 100,
+      ].join(".")
+    end
 end
